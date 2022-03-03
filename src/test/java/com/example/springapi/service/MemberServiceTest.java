0bcd9ea3625853a6510296bee0967f36097d6c48
0BVer <dr0joon@gmail.com>
2022-03-04 02:06:32 +0900

    FEATURE : add MemberServiceTest

--- src/test/java/com/example/springapi/service/MemberServiceTest.java
+++ src/test/java/com/example/springapi/service/MemberServiceTest.java
@@ -1,9 +1,7 @@
 package com.example.springapi.service;
 
 import com.example.springapi.domain.Member;
-import com.example.springapi.repository.MemberRepository;
 import com.example.springapi.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
