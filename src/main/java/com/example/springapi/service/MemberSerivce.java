08d0cf7abef5d774b9b4a9f8c2d12dbff1c07e70
0BVer <dr0joon@gmail.com>
2022-03-04 01:43:19 +0900

    DEBUG : edit MemberSerivce

--- src/main/java/com/example/springapi/service/MemberSerivce.java
+++ src/main/java/com/example/springapi/service/MemberSerivce.java
@@ -8,31 +8,28 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberSerivce {
-    private class MemberService {
-        private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-        // 회원 가입
-        public Long join(Member member) {
-            validateDuplicateMember(member);
-            memberRepository.save(member);
-            return member.getId();
-        }
+    // 회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
 
-        private void validateDuplicateMember(Member member) {
-            memberRepository.findByName(member.getName())
-                    .ifPresent(m -> {
-                        throw new IllegalStateException("이미 존재하는 회원입니다.");
-                    });
-        }
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
 
-        // 전체 회원 조회
-        public List<Member> findMembers() {
-            return memberRepository.findAll();
-        }
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
 
-        public Optional<Member> findOne(Long memberId) {
-            return memberRepository.findById(memberId);
-        }
-
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
