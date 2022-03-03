4eeee721254886879a767122d6a6a563a0221bb0
0BVer <dr0joon@gmail.com>
2022-03-04 00:38:57 +0900

    FEATURE : add test for MemoryMemberRepository

---
+++ src/test/java/com/example/springapi/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.example.springapi.repository;
+
+import com.example.springapi.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach //각 테스트가 끝나면 실행, 테스트의 순서는 랜덤이라 테스트가 끝나면 공용 저장소는 비워주어야 한다.
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result =  repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result =  repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        //when
+        List<Member> result =  repository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
